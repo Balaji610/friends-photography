4712bd97c7728175c513a860ba51832b46115df8
Balaji610 <91706710+Balaji610@users.noreply.github.com>
2025-05-29 12:24:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const viewMoreButtons = document.querySelectorAll(".btn-primary");
+  const modal = document.getElementById("portfolioModal");
+  const modalTitle = modal.querySelector(".modal-title");
+  const modalBody = modal.querySelector(".modal-body img");
+  const modalDescription = modal.querySelector(".modal-body p");
+  const form = document.querySelector("#contact form");
+
+  viewMoreButtons.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+      const card = button.closest(".card");
+      const title = card.querySelector(".card-title").textContent;
+      const imgSrc = card.querySelector(".card-img-top").src;
+      const description = card.querySelector(".card-text").textContent;
+
+      modalTitle.textContent = title;
+      modalBody.src = imgSrc;
+      modalDescription.textContent = description;
+
+      $("#portfolioModal").modal("show");
+    });
+  });
+
+  // Smooth scrolling for links
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    // Form validation and submission logic here
+    alert("Message sent successfully!");
+    
+  });
+});
+function sendmail(){
+  var pa={
+    hname=document.getElementById("name")
+    hemail=document.getElementById("email")
+    hmessage=document.getElementById("message")
+    
+  }
+  emailjs.send("service_jp4gq5l","template_mg97won",pa).then(alert("Email sent!!"))
+}
